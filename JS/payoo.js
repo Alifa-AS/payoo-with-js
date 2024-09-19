43aa3551d80e806b6b74cba89e1b516dd066db02
AlifaSrn <alifa.nub@gmail.com>
2024-09-20 01:42:36 +0600

    first commit

---
+++ JS/payoo.js
@@ -0,0 +1,46 @@
+// console.log('hi')
+
+//search: form submit reloading the page
+
+// step-1: set event handler
+// document.getElementById('button-login').addEventListener('click', function(event){
+//     // step-2: prevent default behavior (prevent reloading browser)
+//     event.preventDefault(); //vejal for beginners
+    
+
+//     // step-3: get the phone number and pin number
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+    
+//     console.log(phoneNumber, pinNumber);
+    
+    // step-4: validate phone and pin
+    // THIS IS TEMPORARY, you should not  do like this
+    // if(phoneNumber === '5' && pinNumber ==='1234'){
+    // console.log('you are logged in');
+    // }
+    // step-5: allow user to use the website
+    // else{ 
+    //     alert('wrong phone number or pin')
+    // }
+// });  
+
+document.getElementById('button-login').addEventListener('click', function(event){
+    event.preventDefault();
+
+    // get phone number & pin
+    const phoneNumber = document.getElementById('phone-number').value
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    // Bad way to validate
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('wrong phone number or pin')
+    }
+       
+ });
+
