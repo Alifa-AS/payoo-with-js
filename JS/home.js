cd8d6b5f1038cd3dd69affa0c12756dfba833099
AlifaSrn <alifa.nub@gmail.com>
2024-09-20 07:06:35 +0600

    added partially the add money event

--- JS/home.js
+++ JS/home.js
@@ -2,10 +2,10 @@
 /**
  * step-1: Event handler 
  * prevent page reload
- * step-2:get money to be added to the account balance
- * step-3:get amount
+ * step-2:get money to be added to the account balance and also get thr pin number
+ * step-3:verify the pin number
  * step-4:get current amount
- * step-5:update amount
+ * step-5:update amount; add money input with balance
  * step-6:main account er ste set 
  */
 
@@ -16,9 +16,32 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
 
     //step-2:get money to be added to the account balance
     const addMoneyInput = document.getElementById('input-add-money').value;
-    console.log(addMoneyInput)
+    console.log(addMoneyInput);
     
-    // get pin
+    // get pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput)
+    console.log(pinNumberInput);
+
+    // step-3:verify the pin number
+    // wrong way to validate pin number
+    if(pinNumberInput === '1234'){
+        console.log('adding money to your account');
+
+        //  step-4:get current amount
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        // step-5:update amount; add money input with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + balanceNumber;
+            console.log(newBalance);
+
+            // step-6:Update the balance in the UI/DOM
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+    else{
+        alert('fail to add money! please try again ');
+    }
+
 })
