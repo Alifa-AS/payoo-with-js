1593d682324317996932847b229fbdd71862ea47
AlifaSrn <alifa.nub@gmail.com>
2024-09-20 06:35:13 +0600

    added partially the add money event

--- JS/home.js
+++ JS/home.js
@@ -1,1 +1,24 @@
-console.log('hi')
+// add money to the account
+/**
+ * step-1: Event handler 
+ * prevent page reload
+ * step-2:get money to be added to the account balance
+ * step-3:get amount
+ * step-4:get current amount
+ * step-5:update amount
+ * step-6:main account er ste set 
+ */
+
+// step-1: add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    //step-2:get money to be added to the account balance
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput)
+    
+    // get pin
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput)
+})
